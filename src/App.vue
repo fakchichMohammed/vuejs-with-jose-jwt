0a38281bb0a755b39ef39893266b9e6573b589b8
Mohammed FAKCHICH <mohammed_fakchich@carrefour.com>
2022-06-02 11:07:13 +0200

    added onUpdated hook

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,38 @@
 <script setup>
-import { ref, onMounted } from 'vue'
-import * as jose from 'jose'
+import { ref, onMounted, onUpdated } from "vue";
+import * as jose from "jose";
 
-const token = ref('')
-const decodedToken = ref('')
-const tokenFromLocalStorage = ref('')
+const token = ref("");
+const decodedToken = ref("");
+const tokenFromLocalStorage = ref("");
 
 function decodeToken() {
-  decodedToken.value = jose.decodeJwt(token.value)
+  decodedToken.value = jose.decodeJwt(token.value);
   console.log(decodedToken.value);
-  window.localStorage.setItem("token", token.value)
+  window.localStorage.setItem("token", token.value);
 }
 
 function getToken() {
-  const token = window.localStorage.getItem('token')
+  const token = window.localStorage.getItem("token");
   if (token !== null) {
-    tokenFromLocalStorage.value = token
+    tokenFromLocalStorage.value = token;
+  } else {
+    tokenFromLocalStorage.value = "No token found!";
   }
 }
 
-onMounted(() => {
-  getToken()
-})
+onMounted(() => { getToken(); });
 
-
+onUpdated(() => { getToken() });
 </script>
 <template>
-  <hr>
-  <h1>Ex : 1 - 2 - 3 </h1>
-  <input type='text' v-model='token' />
-  <button @click='decodeToken'>decode</button>
+  <hr />
+  <h1>Ex : 1 - 2 - 3</h1>
+  <input type="text" v-model="token" />
+  <button @click="decodeToken">decode</button>
   <p>{{ decodedToken }}</p>
-  <hr>
-  <h1>Ex : 4 </h1>
+  <hr />
+  <h1>Ex : 4</h1>
   <p>{{ tokenFromLocalStorage }}</p>
 </template>
 <style>
